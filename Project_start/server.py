4b3a72c85d6539d4866ecbf147bfaaabe2c68b24
sicchio99 <giovannisicchio@gmail.com>
2024-07-17 10:27:42 +0200

    test vari

---
+++ Project_start/server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/start', methods=['GET'])
+def start_script():
+    # Esegui qui il tuo script o operazione
+    # Ad esempio, eseguire uno script shell
+    import subprocess
+    try:
+        result = subprocess.run(['./start.sh'], check=True, text=True, capture_output=True)
+        return f'Script avviato con successo: {result.stdout}', 200
+    except subprocess.CalledProcessError as e:
+        return f'Errore nell\'esecuzione dello script: {e.stderr}', 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5008)
