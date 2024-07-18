bab07f320e8d64bbb8b4bfdcfb19d77099c38739
sicchio99 <giovannisicchio@gmail.com>
2024-07-18 11:02:55 +0200

    Correzione Sense module

--- Robot_modules/Sense_module/sense.py
+++ Robot_modules/Sense_module/sense.py
@@ -5,18 +5,15 @@ from Kobuki import Kobuki
 
 
 class Body:
-    _sensor_array: list
     _d_sensors: dict
     _sim_body: Kobuki
     _sensor_reader: UltrasonicSensorReader
     _sensor_queue: deque
 
-    def __init__(self, sensors):
-        assert isinstance(sensors, list)
+    def __init__(self):
+        # /dev/ttyUSB0 SENSORS
+        # /dev/ttyUSB1 KOBUKI
         self._d_sensors = {}
-        for sensor in sensors:
-            self._d_sensors[sensor] = 0
-        self._sensor_array = list(self._d_sensors.keys())
         self._sim_body = Kobuki()
         self._sensor_reader = UltrasonicSensorReader()
         self._sensor_queue = deque()
@@ -25,13 +22,13 @@ class Body:
         # self._sim_body.kobukistart(self._sim_body.read_data)
 
     def sense(self, client):
-        # Leggere i dati dei sensori a ultrasuoni
+        # Leggere i dati dei sensori a ultrasuoni, nello specifico recupera l'ultima misurazione
         sensor_data = self._sensor_reader.read_sensor_data()
         if sensor_data:
             self._sensor_queue.append(sensor_data)
         while self._sensor_queue:
             sensor_data = self._sensor_queue.popleft()
-            sensor_name = f"ultrasonicSensor[{sensor_data['sensor_id'][1:]}]"  # Assumendo che sensor_id sia del tipo "S1", "S2", ecc.
+            sensor_name = sensor_data['sensor_id']  # Assumendo che sensor_id sia del tipo "S1", "S2", ecc.
             self._d_sensors[sensor_name] = sensor_data['distance']
 
         # Leggere l'orientazione del robot
@@ -41,16 +38,15 @@ class Body:
         # Leggere posizione del robot
 
         # Pubblicare i dati su MQTT
-        for name in self._sensor_array:
+        for name in self._d_sensors.keys():
             client.publish(f"sense/{name}", str(self._d_sensors[name]))
             print(f"Published data from sensor: {name}")
-        # client.publish(f"sense/orientation", str(angle))
+        client.publish(f"sense/orientation", str(angle))
 
 
 if __name__ == "__main__":
-    my_robot = Body(["Vision_sensor", "ultrasonicSensor[0]",
-                    "ultrasonicSensor[4]", "ultrasonicSensor[7]"])
-    # print("Keys in _d_sensors:", my_robot._d_sensors.keys())
+
+    my_robot = Body()
 
     client_pub = mqtt.Client(
         mqtt.CallbackAPIVersion.VERSION2, reconnect_on_failure=True)
